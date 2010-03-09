9d163e848683390dac98de43c532841ca9d0a06e
Paul Ingles <paul@oobaloo.co.uk>
2010-03-09 17:48:59 +0000

    Exclude commons-logging from Esper- use our specified version

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
     [org.clojure/clojure "1.1.0"]
     [org.clojure/clojure-contrib "1.1.0"]
     [commons-logging/commons-logging "1.1.1"]
-    [com.espertech/esper "3.3.0" :exclusions [log4j]]
+    [com.espertech/esper "3.3.0" :exclusions [commons-logging log4j]]
     [com.rabbitmq/amqp-client "1.7.2"]])
